5eaaca3cbdaa5e52cd90f70927db63628e50c5ca
valerio-vaccaro <valerio.vaccaro@gmail.com>
2019-06-11 22:07:14 +0200

    asset registry

--- pub.py
+++ pub.py
@@ -6,6 +6,8 @@ import os
 import time
 import configparser
 import json
+import wallycore as wally
+import struct
 
 app = Flask(__name__, static_url_path='/static')
 qrcode = QRcode(app)
@@ -37,7 +39,6 @@ else:
 host = RPCHost(serverURL)
 if (len(rpcPassphrase) > 0):
     result = host.call('walletpassphrase', rpcPassphrase, 60)
-    print(result)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
@@ -77,6 +78,7 @@ def home():
                     status = os.popen("systemctl start tor").read()
 
         info = host.call('getblockchaininfo')
+
         data = {
             'info': info,
             'torAddr': os.popen("cat "+torBaseDir+"/hostname").read(),
@@ -87,6 +89,7 @@ def home():
             'result' : result,
             'admin' : admin,
         }
+
         return render_template('home', **data)
 
 @app.route('/login', methods = ['POST'])
@@ -95,11 +98,13 @@ def do_admin_login():
         session['logged_in'] = True
     else:
         flash('wrong password!')
+
     return home()
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
+
     return home()
 
 @app.route('/tx', methods = ['GET'])
@@ -120,6 +125,7 @@ def tx():
             'tx' : tx,
             'results' : json.dumps(result, indent=4, sort_keys=True),
         }
+
         return render_template('tx', **data)
 
 @app.route('/blocks', methods = ['GET'])
@@ -133,6 +139,8 @@ def blocks():
 
         max = host.call('getblockcount')
         min = max-10
+        if (min < 0):
+            min = 0
 
         results = []
 
@@ -145,6 +153,7 @@ def blocks():
         data = {
             'results' : results,
         }
+
         return render_template('blocks', **data)
 
 @app.route('/block', methods = ['GET'])
@@ -166,6 +175,7 @@ def block():
             'tx' : id,
             'results' : json.dumps(result, indent=4, sort_keys=True),
         }
+
         return render_template('block', **data)
 
 
@@ -181,22 +191,10 @@ def lbtc():
         result = ""
         assets_sent=[]
         assets_received=[]
-        txs = host.call('listtransactions', '*', 1000000)
-        for tx in txs:
-            item={}
-            item['tx'] = tx['txid'][:8]+"..."
-            item['transaction'] = tx['txid']
-            item['transaction_url'] = "./tx?hex="+tx['txid']
-            item['asset'] = tx['asset']
-            item['amount'] = tx['amount']
-            if item['amount'] > 0 and item['asset'] == policyasset :
-                assets_received.append(item)
-            if item['amount'] < 0 and item['asset'] == policyasset :
-                assets_sent.append(item)
+
         if request.method == 'POST':
             command = request.form.get('command')
             action = request.form.get('action')
-
             if command == 'address':
                 if action == 'new':
                     address = host.call('validateaddress', host.call('getnewaddress'))
@@ -217,6 +215,18 @@ def lbtc():
                     tx = request.form.get('tx')
                     result = host.call('sendrawtransaction', tx)
 
+        txs = host.call('listtransactions', '*', 1000000)
+        for tx in txs:
+            item={}
+            item['tx'] = tx['txid'][:8]+"..."
+            item['transaction'] = tx['txid']
+            item['transaction_url'] = "./tx?hex="+tx['txid']
+            item['asset'] = tx['asset']
+            item['amount'] = tx['amount']
+            if item['amount'] > 0 and item['asset'] == policyasset :
+                assets_received.append(item)
+            if item['amount'] < 0 and item['asset'] == policyasset :
+                assets_sent.append(item)
         balances = host.call('getbalance')
         wallets = host.call('listwallets')
 
@@ -229,6 +239,7 @@ def lbtc():
             'result' : result,
             'wallets' : wallets,
         }
+
         if (rpcChain=='regtest'):
             data['generate'] = True
 
@@ -241,10 +252,81 @@ def asset():
         }
         return render_template('login', **data)
     else:
-        balances = host.call('getbalance')
+
         results=[]
         assets_sent=[]
         assets_received=[]
+
+        command = request.form.get('command')
+        action = request.form.get('action')
+        result = ""
+        if command == 'asset':
+            if action == 'create':
+                res = {}
+                asset_amount = request.form.get('asset_amount')
+                token_amount = request.form.get('token_amount')
+                issuer_pubkey = request.form.get('pubkey')
+                name = request.form.get('name')
+                ticker = request.form.get('ticker')
+                precision = request.form.get('precision')
+                domain = request.form.get('website')
+                version = 0 # don't change
+                blind = False
+                feerate = 0.00003000
+                #tx = host.call('issueasset', asset_amount, token_amount)
+                #result = "Generate "+str(asset_amount)+" of asset "+tx['asset']+" and "+str(token_amount)+" of reissuance tokens"+tx['token']+"."
+                # Create base transaction
+                base = host.call('createrawtransaction', [], [{'data':'00'}])
+                funded = host.call('fundrawtransaction', base, {'feeRate':feerate})
+                # Calculate prevout
+                decoded = host.call('decoderawtransaction', funded['hex'])
+                prev_tx = decoded['vin'][0]['txid']
+                prev_vout = decoded['vin'][0]['vout']
+                res['prev_tx'] = '{}:{}'.format(prev_tx, prev_vout)
+                # Create the contact and calculate the asset id (Needed for asset registry!)
+                contract = json.dumps({'name': name, 'ticker': ticker, 'precision': precision, 'entity': {'domain': domain}, 'issuer_pubkey': issuer_pubkey, 'version': version}, separators=(',',':'), sort_keys=True)
+                contract_hash = wally.hex_from_bytes(wally.sha256(contract.encode('ascii')))
+                a = wally.hex_from_bytes(wally.hex_to_bytes(prev_tx)[::-1])+wally.hex_from_bytes(struct.pack('<L', int(prev_vout)))
+                a = wally.hex_from_bytes(wally.sha256d(wally.hex_to_bytes(a)))
+                b = a + contract_hash
+                merkle = wally.hex_from_bytes(wally.sha256_midstate(wally.hex_to_bytes(b)))
+                c = merkle + '0000000000000000000000000000000000000000000000000000000000000000'
+                merkle = wally.hex_from_bytes(wally.sha256_midstate(wally.hex_to_bytes(c))[::-1])
+                res['asset_id'] = merkle
+                res['contract'] = contract
+                res['contract_hash'] = contract_hash
+                # Create the rawissuance transaction
+                contract_hash_rev = wally.hex_from_bytes(wally.hex_to_bytes(contract_hash)[::-1])
+                rawissue = host.call('rawissueasset', funded['hex'], [{'asset_amount':amount, 'asset_address':asset_address, 'token_amount':reissuance_amount, 'token_address':reissuance_token_address, 'blind':blind, 'contract_hash':contract_hash_rev}])
+                # Blind the transaction
+                blind = host.call('blindrawtransaction', rawissue[0]['hex'], True, [], False)
+                # Sign transaction
+                signed = host.call('signrawtransactionwithwallet', blind)
+                decoded = host.call('decoderawtransaction', signed['hex'])
+                res['decoded_raw_transaction'] = decoded
+                res['raw_transaction'] = signed['hex']
+                # Test transaction
+                test = hos1.call('testmempoolaccept', [signed['hex']])
+                res['testmempoolaccept'] = str(test)
+                if test[0]['allowed'] is True:
+                    txid = host.call('sendrawtransaction', signed['hex'])
+                    res['txid'] = txid
+                    # Import issuance blinding key in the second signer
+                    issuanceblindingkey = host_1.call('dumpissuanceblindingkey', txid, 0)
+                    host_2.call('importissuanceblindingkey', txid, 0, issuanceblindingkey)
+                    res['issuanceblindingkey'] = issuanceblindingkey
+                    res['registry'] = {'asset_id': res['asset_id'], 'contract': json.loads(res['contract']), 'issuance_txin': {'txid': res['txid'], 'vin':0}}
+            if action == 'send':
+                address = request.form.get('address')
+                asset = request.form.get('asset')
+                amount = request.form.get('amount')
+                if host.call('validateaddress', address)['isvalid']:
+                    tx = host.call('sendtoaddress', address, amount, "", "", False, False, 100, "ECONOMICAL", asset)
+                    result = "Sent "+str(amount)+" "+amount+" to address "+address+" with transaction "+tx+"."
+                else:
+                    result = "Wrong address!"
+
+        balances = host.call('getbalance')
         issuances = host.call('listissuances')
         for asset in issuances:
             item={}
@@ -273,25 +355,6 @@ def asset():
             if item['amount'] < 0 and item['asset'] != policyasset and item['asset'] != '0000000000000000000000000000000000000000000000000000000000000000':
                 assets_sent.append(item)
 
-        command = request.form.get('command')
-        action = request.form.get('action')
-        result = ""
-        if command == 'asset':
-            if action == 'create':
-                asset_amount = request.form.get('asset_amount')
-                token_amount = request.form.get('token_amount')
-                tx = host.call('issueasset', asset_amount, token_amount)
-                result = "Generate "+str(asset_amount)+" of asset "+tx['asset']+" and "+str(token_amount)+" of reissuance tokens"+tx['token']+"."
-            if action == 'send':
-                address = request.form.get('address')
-                asset = request.form.get('asset')
-                amount = request.form.get('amount')
-                if host.call('validateaddress', address)['isvalid']:
-                    tx = host.call('sendtoaddress', address, amount, "", "", False, False, 100, "ECONOMICAL", asset)
-                    result = "Sent "+str(amount)+" "+amount+" to address "+address+" with transaction "+tx+"."
-                else:
-                    result = "Wrong address!"
-
         data = {
             'balances' : balances,
             'results': results,
@@ -299,6 +362,7 @@ def asset():
             'assets_received': assets_received,
             'result' : result,
         }
+
         return render_template('asset', **data)
 
 @app.route('/about')
